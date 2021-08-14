3c6f1cf6c1a22027ea365eb4eb30b45efc5eba03
Vinicius Assis <viniciusassis2012@gmail.com>
2021-08-14 12:46:48 -0300

    - Correção na criação de blocos fixos - Alteração de logica de atualização dos blocos

--- Tetris/Assets/Scripts/GameManager.cs
+++ Tetris/Assets/Scripts/GameManager.cs
@@ -4,56 +4,92 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour{
 
+    private IEnumerator coroutine;
+
     private Tetromino[] tetrominos;
     private Block[,] matrix;
-    private List<Block> fixedBlocks;
+
+    private GameObject tilesPivot;
 
     public int numColumns;
     public int numRows;
     public GameObject blockPrefab;
 
+
     // Start is called before the first frame update
-    void Start(){
-        matrix = new Block[numRows, numColumns];
-        fixedBlocks = new List<Block>();
+    void Start()
+    {
+        tilesPivot = new GameObject("Tiles Pivot");
+        matrix     = new Block[numColumns, numRows];
+
         CreateBoundaryBlocks();
 
-    }
-
-    // Update is called once per frame
-    void Update() {
-        UpdatePositions();
-        DrawMatrix();
-    }
-
-    void UpdatePositions(){
+        // Update Tiles once per second
+        coroutine = UpdateTilesRoutine(1.0f);
+        StartCoroutine(coroutine);
 
     }
 
-    void DrawMatrix(){
-        foreach(var item in fixedBlocks){
-            matrix[item.position.x, item.position.y] = item;
-        }
-
-        for (int i = 0; i < numColumns; i++){
-            for (int j = 0; j < numRows; j++){
-                if (j == 0 || j == numRows - 1 || i == 0|| i == numColumns - 1){
-                    Block block = matrix[j,i];
-                    GameObject instance = Instantiate(blockPrefab, 
-                                                      new Vector3(block.position.x,block.position.y,0),
-                                                      Quaternion.identity);
+    void CreateBoundaryBlocks() 
+    {
+        for (int i = 0; i < numColumns; i++)
+        {
+            for (int j = 0; j < numRows; j++)
+            {
+                if (j == 0 || j == numRows - 1 || i == 0 || i == numColumns - 1)
+                {
+                    matrix[i, j] = new Block(new Vector2Int(i, j), Color.white);
                 }
             }
         }
-        
-    }    
+    }
 
-    void CreateBoundaryBlocks(){
-        for (int i = 0; i < numColumns ; i++){
-            for (int j = 0; j < numRows ; j++){
-                if (j == 0 || j == numRows - 1 || i == 0||i == numColumns - 1){
-                    fixedBlocks.Add(new Block(new Vector2Int(j,i), Color.white));
-                }
+    private IEnumerator UpdateTilesRoutine(float refreshTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(refreshTime);
+
+            Debug.Log("Updating Tiles... ");
+
+            UpdatePositions();
+            UpdateTiles();
+
+        }
+    }
+
+    void UpdatePositions()
+    {
+
+    }
+
+    void ClearTiles()
+    {
+        foreach (Transform child in tilesPivot.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void UpdateTiles()
+    {
+
+        ClearTiles();
+
+        for (int i = 0; i < numColumns; i++)
+        {
+            for (int j = 0; j < numRows; j++)
+            {
+                Block block = matrix[i, j];
+
+                if (block == null) continue;
+
+                Vector3 blockPosition = new Vector3(block.position.x, block.position.y, 0);
+                GameObject blockInstance = Instantiate(blockPrefab, blockPosition, Quaternion.identity);
+
+                blockInstance.name = "Tile";
+
+                blockInstance.transform.SetParent(tilesPivot.transform);
             }
         }
     }
